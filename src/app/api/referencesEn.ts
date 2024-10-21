6873953ea49ed83a11b35cf5785f3f60ac12f9f9
abdalrhmanfadel25 <abdalrhmanfadel289@gmail.com>
2024-10-21 04:12:33 +0300

    Initial commit - adding all project files

---
+++ src/app/api/referencesEn.ts
@@ -0,0 +1,24 @@
+export const referencesEn = [
+    {
+        personImg: "assets/images/unknown.jpg",
+        personFullname: 'Velghe Walter',
+        personPosition: "IT Plant Manager",
+        personCompany: 'Agc glass europe',
+        message: "Pioneer Team is very effective in application development and in their maintenance, innovative and rational. Coding like lifestyle, their work features high quality products, following the standards and best practices. they have completed three projects on our site and I am so impressed with their techniques. they always have a thought criticism. they are always ready to take on challenges.",
+    },
+    {
+        personImg: "assets/images/lotfi-benkhider.jpg",
+        personFullname: 'Benkhider Lotfi',
+        personPosition: "CEO & Founder",
+        personCompany: 'Scotfy',
+        message: "Pioneer Team completed their internship in my company.they are  independent and have a great learning ability. they meets delivery deadlines. they have team spirit. Pioneer Team do not hesitate to propose new relevant ideas. they respects the instructions, its code is clean.",
+    },
+    {
+        personImg: "assets/images/mohamed-yahiatene.jpg",
+        personFullname: 'Yahiatene Mohamed',
+        personPosition: "DevOps Engineer",
+        personCompany: 'AG2R La Mondiale',
+        message: "I worked for two years with Pioneer Team at the University of Alexandria. Pioneer team is a very good working partners with high knowledge in backend and frontend development, Pioneer team  alwhays keen to move forward without ever being afraid to use new technologies. they do not hesitate to help their colleagues whenever they can. they manages to solve problems quickly and efficiently with typical optimism.",
+    }
+];
+
